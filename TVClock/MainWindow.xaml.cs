df9bf94f47162d774dd188e692272123e7ec74f5
robin15 <komiyama.tomoaki@gmail.com>
2023-08-29 23:19:11 +0900

    Alt+マウス操作で移動する仕様追加

--- TVClock/MainWindow.xaml.cs
+++ TVClock/MainWindow.xaml.cs
@@ -2,13 +2,17 @@
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Threading;
+using System.Windows.Input;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace TransparentClockApp
 {
     public partial class MainWindow : Window
     {
-        private DispatcherTimer timer;
-
+        private DispatcherTimer timer, timer2;
+        private bool fOnWindowFirst = false;
+        private DateTime time_prev;
         public MainWindow()
         {
             InitializeComponent();
@@ -18,19 +22,27 @@ namespace TransparentClockApp
             timer.Tick += Timer_Tick;
             timer.Start();
             this.MouseEnter += (sebder, e) => {
-                TimeTextBlock.Text = "";
-                timer.Stop();
-            };
-            this.MouseLeave += (sebder, e) => {
-                TimeTextBlock.Foreground = new SolidColorBrush(Colors.White);
-                TimeTextBlock.Text = DateTime.Now.ToString("HH:mm");
-                timer.Start();
+
+                if ((Keyboard.Modifiers & ModifierKeys.Alt) != ModifierKeys.Alt)
+                {
+                    time_prev = DateTime.Now;
+                    if (!fOnWindowFirst)
+                    {
+                        fOnWindowFirst = true;
+                        timer.Stop();
+                        this.Hide();
+                        timer2 = new DispatcherTimer();
+                        timer2.Interval = TimeSpan.FromSeconds(0.2);
+                        timer2.Tick += Timer_Tick2;
+                        timer2.Start();
+                    }
+                }
             };
             this.MouseLeftButtonDown += (sender, e) => {
-                TimeTextBlock.Foreground = new SolidColorBrush(Colors.DimGray);
-                TimeTextBlock.Text = DateTime.Now.ToString("HH:mm");
+                TimeTextBlock.Foreground = new SolidColorBrush(Colors.Gray);
                 this.DragMove();
             };
+            
             this.MouseLeftButtonUp += (sender, e) => {
                 TimeTextBlock.Foreground = new SolidColorBrush(Colors.White);
             };
@@ -39,9 +51,19 @@ namespace TransparentClockApp
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            // 現在の時刻を取得してテキストに反映
             string currentTime = DateTime.Now.ToString("HH:mm");
             TimeTextBlock.Text = currentTime;
+        }
+        private void Timer_Tick2(object sender, EventArgs e)
+        {
+            TimeSpan difference = DateTime.Now - time_prev;
+            if (difference.Seconds > 1.5)
+            {
+                fOnWindowFirst = false;
+                timer.Start();
+                timer2.Stop();
+                this.Show();
+            }
         }
     }
 }
